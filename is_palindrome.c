192e2de44d0d0f192470d2669b77e90f8fb9d04b
Robert Dragan <robertdragan2020@gmail.com>
2022-09-30 15:48:01 +0200

    Created is_palindrome

---
+++ is_palindrome.c
@@ -0,0 +1,32 @@
+/*
+* Author: Robert Dragan
+* Github: nucata
+* Date:  
+* Description: This program checks if a given string is or not palindrome. If it is returns 1 and prints "Yeey". Else returns 0 and prints "NO, No, n..."
+*/
+
+#include <stdio.h>
+#include "functions.h"
+
+int	is_palindrome(char *s) {
+	int	length = ft_strlen(s);
+
+	for (int i = 0; i < length / 2; i++) {
+		if (s[i] != s[length - 1 - i]) {
+			printf("NO, No, n...\n");
+			return (0);
+		}
+	}
+	printf("Yeey\n");
+	return (1);
+}
+
+int	main(void) {
+	is_palindrome("Holai"); // No...
+	is_palindrome("HooH"); // Yeey
+	is_palindrome("1221"); // Yeey
+	is_palindrome("~~19204"); // No...
+	is_palindrome("~~00~~00~~"); // Yeey
+	return (0);
+}
+
