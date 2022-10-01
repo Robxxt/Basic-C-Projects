71eb77de6c3d2795276fc0a6fc618858eefb139f
Robert Dragan <robertdragan2020@gmail.com>
2022-10-01 12:34:19 +0200

    Created the flip

---
+++ flip_characters.c
@@ -0,0 +1,34 @@
+/*
+* Author: Robert Dragan
+* Github: nucata
+* Date: 01.10.2022  
+* Description: This program receives a string and reverses lowercase characters to uppercase and vice-versa.
+*/
+
+#include <stdio.h>
+
+
+char	*flip(char *str) {
+	int	index;
+
+	while (str[index] != '\0') {
+		// Check if the char is alphabetic lowecase, if yes, it flips it
+		if (str[index] <= 122 && str[index] >= 97)
+			str[index] -= 32;
+		// Check if the char is alphabetic uppercase, if yes, it flips it
+		else if (str[index] <= 90 && str[index] >= 65)
+			str[index] += 32;
+		index++;
+	}
+	return (str);
+}
+
+int	main(void) {
+	char	str[] = "Hi1~ % World";
+	
+	printf("Original:\n %s\n\n", str);
+	flip(str);
+	printf("After fliping:\n %s\n", str);
+	return (0);
+}
+
