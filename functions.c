1ad83a0ac5cc35fce68cb711c222596036ac0703
Robert Dragan <robertdragan2020@gmail.com>
2022-10-02 09:44:15 +0200

    Added fail

--- functions.c
+++ functions.c
@@ -5,10 +5,27 @@
 * Description: This functions are made to be reused in any other project.
 */
 
+#include <stdlib.h>	//exit
+
+void	fail() {
+	exit(EXIT_FAILURE);
+}
+
 int	ft_strlen(char *str) {
 	int	index = 0;
 
 	while (str[index] != '\0')
 		index++;
 	return (index);
+}
+
+int	is_prime(int n) {
+	int	i = 2;
+
+	if (n < i)
+		return (0);
+	while (i <= (n / 2)) {
+		if (n % i == 0)
+			return (0);
+	}
 }
--- functions.h
+++ functions.h
@@ -1,6 +1,9 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+
+void	fail();
 int	ft_strlen(char *str);
+int	is_prime(int n);
 
 #endif
