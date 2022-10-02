159b7b78dd139396d7f23e5b5e657f0ce00c00cc
Robert Dragan <robertdragan2020@gmail.com>
2022-10-02 15:46:52 +0200

    Added ft_putchar & ft_putstr

--- functions.c
+++ functions.c
@@ -6,10 +6,7 @@
 */
 
 #include <stdlib.h>	//exit
-
-void	fail() {
-	exit(EXIT_FAILURE);
-}
+#include <unistd.h>	//write
 
 int	ft_strlen(char *str) {
 	int	index = 0;
@@ -17,6 +14,34 @@ int	ft_strlen(char *str) {
 	while (str[index] != '\0')
 		index++;
 	return (index);
+}
+
+void	ft_putchar(char c) {
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str) {
+	int	str_len = ft_strlen(str);
+	write(1, str, str_len);
+}
+
+float	ft_pow(int base, int pow) {
+	if (base == 0 && pow == 0)
+		return (1);
+	else if (base == 0)
+		return (0);
+	else if (pow == 0)
+		return (1);
+	else if (base == 1)
+		return (1);
+	else if (pow > 0)
+		return (base * ft_pow(base, pow - 1));
+	else if (pow < 0)
+		return (1.00 / base * ft_pow(base, pow + 1));
+}
+
+void	fail() {
+	exit(EXIT_FAILURE);
 }
 
 int	is_prime(int n) {
--- functions.h
+++ functions.h
@@ -1,9 +1,11 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-
-void	fail();
 int	ft_strlen(char *str);
+void	ft_putchar(char c);
+void	ft_printstr(char *str);
+float	ft_pow(int base, int pow);
+void	fail();
 int	is_prime(int n);
 
 #endif
